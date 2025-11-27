1ea174cd460b7847d21ff34e061782283e2245fa
CreativeSeba <laganseba@gmail.com>
2025-11-27 11:13:39 +0100

    fix github pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { HashRouter } from "react-router-dom"; // Use HashRouter for GitHub Pages
 import "./index.css";
 import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter> {/* Use HashRouter for routing */}
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
